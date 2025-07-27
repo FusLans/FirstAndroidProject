a84c32a01b5d8b370f579d3789d1264d5a48345a
Максим <md.botin@gmail.com>
2025-07-27 17:34:06 +0300

    Заметки

--- app/src/main/java/example/project/test/MainViewModel.kt
+++ app/src/main/java/example/project/test/MainViewModel.kt
@@ -1,16 +1,19 @@
 package example.project.test
 
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
-    var textNow by mutableStateOf("")
+    var list = mutableStateListOf("")
         private set
+    var newValue by mutableStateOf("")
+    fun onNewList(newListNow: String) {
 
-    fun onNewText(newTextNow: String) {
-        textNow = newTextNow
+        list.add(newListNow)
     }
 
 }
